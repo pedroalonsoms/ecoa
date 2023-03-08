b36a5514bd8036a105de83386aa983a11cd94852
ben10pedrin <pedro123ben10@gmail.com>
2023-03-07 22:02:15 -0600

    first db schema design

--- README.md
+++ README.md
@@ -24,6 +24,12 @@ Team **2**
 
 # References & Credits
 
+- Chris Kolmenic MySQL [ENUM's Article](https://komlenic.com/244/8-reasons-why-mysqls-enum-data-type-is-evil/)
+- Table Inheritance [Article](https://www.freecodecamp.org/news/single-table-inheritance-vs-polymorphic-associations-in-rails-af3a07a204f2/amp/)
+- [Class Table Ineritance](https://www.martinfowler.com/eaaCatalog/classTableInheritance.html)
+- [Single Table Ineritance](https://www.martinfowler.com/eaaCatalog/singleTableInheritance.html)
 - [MiduDev](https://www.youtube.com/@midudev)
 - [W3Schools](https://www.w3schools.com/)
+- [StackExchange](https://stackexchange.com/)
+- [StackOverflow](https://stackoverflow.com/)
 - [MySQLTutorial](https://www.mysqltutorial.org/)
--- server/src/db/init.sql
+++ server/src/db/init.sql
@@ -1,56 +1,86 @@
+--- Abstract tables
+
+CREATE TABLE evaluable_kind (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(64) NOT NULL,
+
+    PRIMARY KEY(id)
+    -- enum {TEACHER, DIRECTOR, STUDENT, MENTOR, COURSE, FACILITIES}
+    -- TODO: what happens if a director is also a teacher?
+    -- TODO: are we going to handle MENTOR cases?
+);
+
+CREATE TABLE evaluable (
+    id INT AUTO_INCREMENT NOT NULL,
+    evaluable_kind_id INT NOT NULL,
+    
+    PRIMARY KEY(id),
+    FOREIGN KEY(evaluable_kind_id) REFERENCES evaluable_kind(id) ON DELETE CASCADE
+    -- TODO: do we even need another table to handle ENUMS and referential integrity?
+);
+
+--- People
+
 CREATE TABLE user (
-    id CHAR(9) NOT NULL,
+    id INT NOT NULL,
+    registration CHAR(9) UNIQUE NOT NULL,
     full_name VARCHAR(64) NOT NULL,
     email VARCHAR(64) NOT NULL,
-    password VARCHAR(64) NOT NULL
-
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE colaborator (
-    id CHAR(9) NOT NULL,
-    role VARCHAR(64) NOT NULL,
-
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES user(id) ON DELETE CASCADE
-);
-
-CREATE TABLE student (
-    id CHAR(9) NOT NULL,
+    password VARCHAR(64) NOT NULL,
     answer_timestamp DATETIME,
 
     PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES user(id) ON DELETE CASCADE
+    FOREIGN KEY (id) REFERENCES evaluable(id) ON DELETE CASCADE
+    -- TODO: handle referential integrity where I could assign "FACILITIES" to a user
 );
 
-CREATE TABLE question (
-    id CHAR(3) NOT NULL,
-    name VARCHAR(512) NOT NULL,
-);
+--- Courses
 
 CREATE TABLE course (
-    id CHAR(6) NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(256) NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES evaluable(id) ON DELETE CASCADE
 );
 
------
+CREATE TABLE enrolled (
+    course_id INT NOT NULL,
+    teacher_id INT NOT NULL,
+    student_id INT NOT NULL,
 
-CREATE TABLE colaborator_answers (
-    student_id CHAR(9) NOT NULL,
-    question_id CHAR(3) NOT NULL,
-    colaborator_id CHAR(9) NOT NULL,
-    score INT NOT NULL
-)
+    PRIMARY KEY (course_id, teacher_id, student_id),
+    FOREIGN KEY (course_id) REFERENCES course(id) ON DELETE CASCADE,
+    FOREIGN KEY (teacher_id) REFERENCES user(id) ON DELETE CASCADE,
+    FOREIGN KEY (student_id) REFERENCES user(id) ON DELETE CASCADE
+    -- TODO: handle referential integrity where student_id and teacher_id must be indeed "STUDENT" and "TEACHER"
+    -- TODO: handle ON DELETE CASCADE edge cases
+);
 
-CREATE TABLE course_answers (
-    student_id CHAR(9) NOT NULL,
-    question_id CHAR(3) NOT NULL,
-    course_id CHAR(9) NOT NULL,
-    score INT NOT NULL
-)
+--- Survey
 
-CREATE TABLE facilities_answers (
-    student_id CHAR(9) NOT NULL,
-    question_id CHAR(3) NOT NULL,
-    score INT NOT NULL
-)
+CREATE TABLE question (
+    id INT AUTO_INCREMENT NOT NULL,
+    evaluable_kind_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (evaluable_kind_id) REFERENCES evaluable_kind(id) ON DELETE CASCADE
+    -- TODO: change to CHAR(3)? F01, F02, etc
+);
+
+CREATE TABLE answers (
+    user_id INT NOT NULL,
+    question_id INT NOT NULL,
+    score INT,
+    comment VARCHAR(2048),
+
+    PRIMARY KEY (user_id, question_id),
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+    FOREIGN KEY (question_id) REFERENCES question(id) ON DELETE CASCADE
+    -- TODO: integrity is destroyed if we add both a score and a comment
+    -- TODO: what prevents us from answering a number-question with text instead of a number?
+);
+
+-- Sample Data
+-- TODO: create a bunch of dummy data for demo purposes
