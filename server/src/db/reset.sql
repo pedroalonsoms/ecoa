725bdc3395cb3e3ffe104e6fd48fd017c67b312f
ben10pedrin <pedro123ben10@gmail.com>
2023-03-30 15:28:28 -0600

    add a few dummy data

--- server/src/db/reset.sql
+++ server/src/db/reset.sql
@@ -18,40 +18,40 @@ DROP TABLE IF EXISTS Colaborator;
 DROP TABLE IF EXISTS Student;
 
 CREATE TABLE Student (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT,
     email VARCHAR(64) UNIQUE NOT NULL,
     pass VARCHAR(32) NOT NULL,
     registration CHAR(9) UNIQUE NOT NULL,
     fullName VARCHAR(64) NOT NULL,
-    campusId CHAR(3) NOT NULL,
+    campus CHAR(3) NOT NULL,
 
     PRIMARY KEY (id)
 );
 
 CREATE TABLE Colaborator (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT,
     email VARCHAR(64) UNIQUE NOT NULL,
     pass VARCHAR(32) NOT NULL,
     registration CHAR(9) UNIQUE NOT NULL,
     fullName VARCHAR(64) NOT NULL,
-    campusId CHAR(3) NOT NULL,
+    campus CHAR(3) NOT NULL,
 
     PRIMARY KEY (id)
 );
 
 CREATE TABLE Teacher (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT,
     email VARCHAR(64) UNIQUE NOT NULL,
     pass VARCHAR(32) NOT NULL,
     registration CHAR(9) UNIQUE NOT NULL,
     fullName VARCHAR(64) NOT NULL,
-    campusId CHAR(3) NOT NULL,
+    campus CHAR(3) NOT NULL,
 
     PRIMARY KEY (id)
 );
 
 CREATE TABLE Course (
-    id INT NOT NULL,
+    id INT AUTO_INCREMENT,
     title VARCHAR(64) NOT NULL,
     kind VARCHAR(32) NOT NULL,
 
@@ -59,7 +59,7 @@ CREATE TABLE Course (
 );
 
 CREATE TABLE Classroom (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT,
     code INT NOT NULL,
     CRN INT NOT NULL,
     courseId INT NOT NULL,
@@ -87,7 +87,7 @@ CREATE TABLE Teaches (
 );
 
 CREATE TABLE Question (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT,
     title VARCHAR(512) NOT NULL,
     kind VARCHAR(32) NOT NULL,
 
@@ -183,7 +183,7 @@ CREATE TABLE TmpCourseComment (
 );
 
 CREATE TABLE Survey (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT,
     title VARCHAR(64) NOT NULL,
     isActive BOOLEAN NOT NULL,
 
@@ -198,3 +198,21 @@ CREATE TABLE SurveyQuestion (
     FOREIGN KEY (surveyId) REFERENCES Survey (id),
     FOREIGN KEY (questionId) REFERENCES Question (id)
 );
+
+INSERT INTO Student VALUES (NULL, 'A01741437@tec.mx', 'pedro', 'A01741437', 'Pedro Alonso Moreno Salcedo', 'MTY');
+INSERT INTO Student VALUES (NULL, 'A00827581@tec.mx', 'kerim', 'A00827581', 'Kerim Taray Malagon', 'MTY');
+
+INSERT INTO Colaborator VALUES (NULL, 'rafadavalos@tec.mx', 'rafa', 'L00000001', 'Rafael Emilio Dávalos', 'MTY');
+
+INSERT INTO Teacher VALUES (NULL, 'csalazar@tec.mx', 'carmen', 'L00000002', 'Carmen Leticia Salazar Cano', 'MTY');
+
+INSERT INTO Course VALUES (NULL, 'Construcción de software y toma de decisiones', 'BLOQUE');
+
+INSERT INTO Classroom VALUES (NULL, 404, 112233, 1);
+
+INSERT INTO Question VALUES (NULL, 'El profesor(a) muestra dominio y experiencia en los temas de la Materia', 'PROFESOR');
+INSERT INTO Question VALUES (NULL, 'El profesor(a) me retó para dar lo mejor de mí(desarrollar nuevas habilidades, nuevos conceptos e ideas, pensar de manera diferente, etc.)', 'PROFESOR');
+INSERT INTO Question VALUES (NULL, '¿Qué le comentarías a un estudiante que quisiera inscribir la Materia con este(a) profesor(a)?', 'PROFESOR');
+
+INSERT INTO Question VALUES (NULL, 'Los temas, las actividades y la situación-problema durante la Materia: (A) Me permitieron aprender y desarrollarme.', 'MATERIA');
+INSERT INTO Question VALUES (NULL, 'Los temas, las actividades y la situación-problema durante la Materia: (B) Son aplicables y de valor.', 'MATERIA');
