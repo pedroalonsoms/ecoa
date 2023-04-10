996cf0cfbfac6dca5211a474879474c378605c8c
ben10pedrin <pedro123ben10@gmail.com>
2023-04-09 23:18:12 -0600

    added temporary iframe for the demo

--- web/src/pages/StudentPage.jsx
+++ web/src/pages/StudentPage.jsx
@@ -1,20 +1,20 @@
-import Styles from './StudentPage.module.css';
+import Styles from "./StudentPage.module.css";
 
-import Navbar from '../components/Navbar';
-import { useLocation } from 'react-router-dom';
+import Navbar from "../components/Navbar";
+import { useLocation } from "react-router-dom";
 
 const StudentPage = () => {
-    const activeLinks = false;
-    const location = useLocation();
-    const data = location.state;
-    console.log(data);
-    return (
-        <div>
-            <Navbar showLinks={activeLinks} />
-            <h2>Bienvenido a la ECOA {data}</h2>
-            <div className={Styles.videojuego}></div>
-        </div>
-    );
+  const activeLinks = false;
+  const location = useLocation();
+  const data = location.state;
+  console.log(data);
+  return (
+    <div>
+      <Navbar showLinks={activeLinks} />
+      <h2>Bienvenido a la ECOA {data}</h2>
+      <iframe className={Styles.videojuego} src="/game"></iframe>
+    </div>
+  );
 };
 
 export default StudentPage;
