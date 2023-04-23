bfb146eed3e618209dbdddafab61e9e6592d218d
Adrián Ramírez <85772081+adrian-rmz@users.noreply.github.com>
2023-04-22 19:50:29 -0600

    Implemented Delete Survey

--- web/src/components/Survey.jsx
+++ web/src/components/Survey.jsx
@@ -1,45 +1,46 @@
 import Styles from "./Survey.module.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    faTrash,
-    faPenToSquare,
-} from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 
 import axios from "axios";
 import { useState } from "react";
 
 const Survey = (props) => {
-    const handleDelete = async (id) => {
-        try {
-            const res = await axios.delete(
-                `http://localhost:8080/api/surveys/${id}`
-            );
-            console.log(res);
-            window.location.reload();
-        } catch (err) {
-            console.log(err);
-        }
-    };
+	const handleDelete = async (id) => {
+		try {
+			const res = await axios.delete(
+				`http://localhost:8080/api/surveys/${id}`
+			);
+			console.log(res);
+			window.location.reload();
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-    
-    return (
-        <div className={Styles.survey}>
-            <p>{props.data.title}</p>
-            <div className={Styles.container}>
-                {props.data.isActive ? <p>Activa</p> : <p>Inactiva</p>}
-                <p>{props.data.startDate} - {props.data.endDate}</p>
-                <div className={Styles.buttons}>
-                    <button className={Styles.button}>
-                        <FontAwesomeIcon icon={faTrash} />
-                    </button>
-                    <button className={Styles.button}>
-                        <FontAwesomeIcon icon={faPenToSquare} />
-                    </button>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className={Styles.survey}>
+			<p>{props.data.title}</p>
+			<div className={Styles.container}>
+				{props.data.isActive ? <p>Activa</p> : <p>Inactiva</p>}
+				<p>
+					{props.data.startDate} - {props.data.endDate}
+				</p>
+				<div className={Styles.buttons}>
+					<button
+						className={Styles.button}
+						onClick={() => handleDelete(props.data.id)}
+					>
+						<FontAwesomeIcon icon={faTrash} />
+					</button>
+					<button className={Styles.button}>
+						<FontAwesomeIcon icon={faPenToSquare} />
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Survey;
