cae5b6d8a2d0ee8e798bd90dfdf84e1320971b11
ben10pedrin <pedro123ben10@gmail.com>
2023-04-27 00:41:31 -0600

    change  to  on questions

--- server/src/db/sql-files/08-questions.sql
+++ server/src/db/sql-files/08-questions.sql
@@ -1,8 +1,8 @@
 INSERT INTO Question VALUES
-(NULL, 'DOM', 'EBDOM', 'El profesor(a) muestra dominio y experiencia en los temas de la Materia:', 'TEACHER', 'NUMERIC'),
-(NULL, 'RET', 'EBRET', 'El profesor(a) me retó para dar lo mejor de mi (desarrollar nuevas habilidades,nuevos conceptos e ideas, pensar de manera diferente, etc.):', 'TEACHER', 'NUMERIC'),
+(NULL, 'DOM', 'EBDOM', 'El profesor(a) muestra dominio y experiencia en los temas de la Unidad de Formación:', 'TEACHER', 'NUMERIC'),
+(NULL, 'RET', 'EBRET', 'El profesor(a) me retó para dar lo mejor de mi (desarrollar nuevas habilidades, nuevos conceptos e ideas, pensar de manera diferente, etc.):', 'TEACHER', 'NUMERIC'),
 (NULL, 'REC', 'E REC', 'En general, mi experiencia de aprendizaje con el profesor(a) fue:', 'TEACHER', 'NUMERIC'),
-(NULL, 'COM', 'EBCOMEN', '¿Qué le comentarías a un estudiante que quisiera inscribir la Materia con este(a) profesor(a)?', 'TEACHER', 'TEXT'),
+(NULL, 'COM', 'EBCOMEN', '¿Qué le comentarías a un estudiante que quisiera inscribir la Unidad de Formación con este(a) profesor(a)?', 'TEACHER', 'TEXT'),
 (NULL, 'ASE', 'EBASE', 'El profesor(a) promovió un ambiente de confianza y respeto:', 'TEACHER', 'NUMERIC'),
 (NULL, 'MET', 'EBMET', 'El acompañamiento que recibí por parte de mi profesor(a) fue adecuado (respuestas a dudas, asesoría, retroalimentación, etc.):', 'TEACHER', 'NUMERIC'),
 (NULL, 'APD', 'EMAPD', '¿Cómo evalúas los siguientes enunciados relacionados con la Materia? Los temas, las actividades y la situación-problema durante la Materia: A) Me permitieron aprender y desarrollarme', 'COURSE', 'NUMERIC'),
